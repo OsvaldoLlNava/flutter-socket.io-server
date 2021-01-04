7415d0a5c6ac45fe5ff0950d0e68bf5f4755bef7
Osvaldo Ll. Nava <luis_llanesn@hotmail.com>
2021-01-04 15:48:47 -0600

    fin seccion de creacion de servidor basico

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+//App de Express
+const app = express();
+
+// Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+
+
+require('./sockets/socket');
+
+
+//path publico
+const publicPath = path.resolve(__dirname, 'public');
+
+// para que se use el path que se acaba de crear
+
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en el puerto', process.env.PORT);
+})
